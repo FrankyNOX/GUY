5e8e4b416a9827c405b5916ec82e221cb098362a
Francois NOX <noah0cnoumbo.com>
2020-10-18 03:59:50 +0200

    Test de mise a jour

--- resources/views/Admin/Dashboard.blade.php
+++ resources/views/Admin/Dashboard.blade.php
@@ -46,22 +46,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 col-12">
-                <div class="card">
-                    <div class="card-header d-flex flex-column align-items-start pb-0">
-                        <div class="avatar bg-rgba-warning p-50 m-0">
-                            <div class="avatar-content">
-                                <i class="feather icon-package text-warning font-medium-5"></i>
-                            </div>
-                        </div>
-                        <h2 class="text-bold-700 mt-1 mb-25">97.5K</h2>
-                        <p class="mb-0">Orders Received</p>
-                    </div>
-                    <div class="card-content">
-                        <div id="orders-received-chart"></div>
-                    </div>
-                </div>
-            </div>
+            
             <div class="col-lg-3 col-md-6 col-12">
                 <div class="card">
                     <div class="card-header d-flex flex-column align-items-start pb-0">
